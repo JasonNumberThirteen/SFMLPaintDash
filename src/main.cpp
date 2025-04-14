ec359926d5c26c52ac8d04c67fc854a1fa9c31db
JasonNumberThirteen <jasonxiii97@gmail.com>
2025-04-14 14:28:29 +0200

    Center the game logo sprite in the game window

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,13 @@
 int main()
 {
 	auto window = sf::RenderWindow(sf::VideoMode({constants::GAME_WIDTH, constants::GAME_HEIGHT}), constants::GAME_TITLE, sf::Style::Default, sf::State::Windowed);
+	auto windowSize = window.getSize();
 	auto gameLogoTexture = sf::Texture("assets/images/mainMenu/gameLogo.png");
+	auto gameLogoTextureSize = gameLogoTexture.getSize();
 	auto gameLogoSprite = sf::Sprite(gameLogoTexture);
 
 	window.setFramerateLimit(144);
+	gameLogoSprite.setPosition(sf::Vector2f((windowSize.x - gameLogoTextureSize.x) >> 1, (windowSize.y - gameLogoTextureSize.y) >> 1));
 
 	while (window.isOpen())
 	{
