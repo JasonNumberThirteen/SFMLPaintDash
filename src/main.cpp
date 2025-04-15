159bc7decc3cb12f1fb3c14bf7c09d8df96198d3
JasonNumberThirteen <jasonxiii97@gmail.com>
2025-04-15 16:58:16 +0200

    Add drawing of the press any key text

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@ int main()
 {
 	auto window = sf::RenderWindow(sf::VideoMode({constants::GAME_WIDTH, constants::GAME_HEIGHT}), constants::GAME_TITLE, sf::Style::Default, sf::State::Windowed);
 	auto windowSize = window.getSize();
+	auto gameFont = sf::Font("assets/fonts/nationalPark.ttf");
+	auto pressAnyKeyText = sf::Text(gameFont);
 	auto gameLogoTexture = sf::Texture("assets/images/mainMenu/gameLogo.png");
 	auto cursorTexture = sf::Texture("assets/images/cursor.png");
 	auto gameLogoTextureSize = gameLogoTexture.getSize();
@@ -14,6 +16,13 @@ int main()
 	window.setFramerateLimit(144);
 	window.setMouseCursorVisible(false);
 	gameLogoSprite.setPosition(sf::Vector2f((windowSize.x - gameLogoTextureSize.x) >> 1, (windowSize.y - gameLogoTextureSize.y) >> 1));
+	pressAnyKeyText.setString("Press any key");
+	pressAnyKeyText.setCharacterSize(32);
+
+	auto pressAnyKeyTextRect = pressAnyKeyText.getLocalBounds();
+
+	pressAnyKeyText.setOrigin(sf::Vector2f(pressAnyKeyTextRect.position.x + pressAnyKeyTextRect.size.x*0.5f, pressAnyKeyTextRect.position.y + pressAnyKeyTextRect.size.y*0.5f));
+	pressAnyKeyText.setPosition(sf::Vector2f(windowSize.x*0.5f, (windowSize.y + gameLogoTextureSize.y)*0.5f + 64));
 
 	while (window.isOpen())
 	{
@@ -30,6 +39,7 @@ int main()
 		cursorSprite.setPosition(static_cast<sf::Vector2f>(mousePosition));
 		window.clear(constants::BACKGROUND_COLOR);
 		window.draw(gameLogoSprite);
+		window.draw(pressAnyKeyText);
 		window.draw(cursorSprite);
 		window.display();
 	}
