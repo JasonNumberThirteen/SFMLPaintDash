eeec681a6045c3ff85526d40b352368703ae5d21
JasonNumberThirteen <jasonxiii97@gmail.com>
2025-04-15 16:29:47 +0200

    Add drawing of the custom cursor sprite

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,13 @@ int main()
 	auto window = sf::RenderWindow(sf::VideoMode({constants::GAME_WIDTH, constants::GAME_HEIGHT}), constants::GAME_TITLE, sf::Style::Default, sf::State::Windowed);
 	auto windowSize = window.getSize();
 	auto gameLogoTexture = sf::Texture("assets/images/mainMenu/gameLogo.png");
+	auto cursorTexture = sf::Texture("assets/images/cursor.png");
 	auto gameLogoTextureSize = gameLogoTexture.getSize();
 	auto gameLogoSprite = sf::Sprite(gameLogoTexture);
+	auto cursorSprite = sf::Sprite(cursorTexture);
 
 	window.setFramerateLimit(144);
+	window.setMouseCursorVisible(false);
 	gameLogoSprite.setPosition(sf::Vector2f((windowSize.x - gameLogoTextureSize.x) >> 1, (windowSize.y - gameLogoTextureSize.y) >> 1));
 
 	while (window.isOpen())
@@ -22,8 +25,12 @@ int main()
 			}
 		}
 
+		auto mousePosition = sf::Mouse::getPosition(window);
+
+		cursorSprite.setPosition(static_cast<sf::Vector2f>(mousePosition));
 		window.clear(constants::BACKGROUND_COLOR);
 		window.draw(gameLogoSprite);
+		window.draw(cursorSprite);
 		window.display();
 	}
 }
